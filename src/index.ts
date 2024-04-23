5af2e87ddd83f58cf7801885fbf2abdcb69bdcc9
tmastrom <thomas.mastromonaco@gmail.com>
2024-04-23 05:58:44 -0700

    chore: use files glob

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
-import fs from "node:fs";
-import path from "node:path";
-
 export const files = import.meta.glob("./files/*.yaml");
-
-export const mega = fs.readFileSync(
-  path.resolve(__dirname, "./files/mega.yaml"),
-  "utf-8"
-);
-
-export const bbccouk = fs.readFileSync(
-  path.resolve(__dirname, "./files/bbccouk.yaml"),
-  "utf-8"
-);
-
-export const webflowcom = fs.readFileSync(
-  path.resolve(__dirname, "./files/webflowcom.yaml"),
-  "utf-8"
-);
